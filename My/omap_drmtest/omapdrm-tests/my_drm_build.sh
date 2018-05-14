1c6ae9667fb31001aefacef5bf4bfe6d9486e352
Athos <renhard@163.com>
2018-05-14 23:27:56 +0800

    construct a new project

--- My/omap_drmtest/omapdrm-tests/my_drm_build.sh
+++
@@ -1,9 +0,0 @@
-
-export PATH=/home/user/gcc-linaro-arm-linux-gnueabihf-4.7-2013.03-20130313_linux/bin:$PATH
-
-
-
-export PKG_CONFIG_PATH=/media/rootfs/usr/lib/pkgconfig
-
-make all
-sleep 5
--- My/omap_drmtest/omapdrm-tests/test.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TEST_H_
-#define _TEST_H_
-
-#include "common.h"
-#include "common-drm.h"
-#include "common-modeset.h"
-#include "common-drawing.h"
-
-#endif
